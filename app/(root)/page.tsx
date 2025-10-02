869bfc8fa841e907af10556f0f41522d099d9c7b
Wilson Chan <wilsonchan54@gmail.com>
2025-10-01 23:58:54 -0400

    Created root route group along with layout.tsx inside / moved page.tsx into root route group / updated app layout.tsx

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Halal or Nah</Button>
+    </div>
+  )
+}
+
+export default Home
