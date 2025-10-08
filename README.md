8f7f573622f9ae247b88ba1ff1d50f49ae2545ad
Wilson Chan <wilsonchan54@gmail.com>
2025-10-07 23:55:44 -0400

    Added styling in UserDropdown component / updated README.md

--- README.md
+++ README.md
@@ -22,3 +22,17 @@
     <a href="https://github.com/WackyChomp/smarty/issues">Request Feature</a>
   </p>
 </div>
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[Next.js]: https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white
+[Next-url]: https://nextjs.org/
+[TypeScript]: https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white
+[TypeScript-url]: https://www.typescriptlang.org/
+
+[React.js]: https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
+[React-url]: https://reactjs.org/
+[Tailwind.css]: https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white
+[Tailwind-url]: https://tailwindcss.com/
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import { Button } from './ui/button'
 import {
   Avatar,
@@ -17,25 +18,42 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 const UserDropdown = () => {
+  const router = useRouter();
+
+  const handleSignOut = async() => {
+    router.push('/sign-in')
+  }
+
+  const mockUser = {name: 'Billy' , email: 'billyboe@gmail.com'}
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' className='flex items-center gap-3 text-yellow-900 hover:text-yellow-500'>
-          <Avatar>
+          <Avatar className='h-8 w-8'>
             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarFallback className='bg-red-500 text-red-950 text-sm font-bold'>
+              {mockUser.name[0]}
+            </AvatarFallback>
           </Avatar>
+
+          <div className="hidden md:flex flex-col items-start">
+            <span className='text-base font-medium text-gray-500'>
+              {mockUser.name}
+            </span>
+          </div>
         </Button>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      <DropdownMenuContent className='text-gray-400'>
+        <div className="flex relative items-center gap-4 py-2">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>      
+        </div>
       </DropdownMenuContent>
     </DropdownMenu>
   )
