798cb4adc96d4c47322f495487f50362acc50eb5
Wilson Chan <wilsonchan54@gmail.com>
2025-10-10 21:58:28 -0400

    Updated useEffect and moved from TradingViewWidget component to hook useTradingViewWidget / updated README.md

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <!-- PROJECT LOGO -->
 <br />
 <div align="center" id='readme-top'>
-  <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3d0d3duanZkZngwdGV5cDMxZGk2NTJ2dHI4NnczaWphdnI1aW1zYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iYTXFJofI7I987H92k/giphy.gif" alt="Logo" target='blank' width="350" height="200">
+  <img src="https://i.giphy.com/iYTXFJofI7I987H92k.webp" alt="Logo" target='blank' width="350" height="200">
 
   <h3 align="center">Smarty</h3>
 
--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -9,19 +9,6 @@ import React, { useEffect, useRef, memo } from 'react';
 const TradingViewWidget = () => {
   const container = useRef(null);
 
-  //turn into reusable hook to render different charts
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = ``
-      container.current.appendChild(script);
-    },
-    []
-  );
-
   return (
     <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
       <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
