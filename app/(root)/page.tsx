d84c3d34944af7706908b452a991719ff17fc9c4
Wilson Chan <wilsonchan54@gmail.com>
2025-10-11 20:50:11 -0400

    Added interface and props in TradingViewWidget component / rendered TradingViewWidget in app page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@/components/ui/button"
+import TradingViewWidget from "@/components/TradingViewWidget"
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants"
+
+// chart types
+// https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js
+
 
 const Home = () => {
   return (
     <div className="flex min-h-screen home_wrapper">
-      <Button>Halal or Nah</Button>
+      <section className="grid w-full gap-8 home_section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget 
+            title='General Market'
+            scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js'
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className=""
+            height={600}
+          />
+        </div>
+        <Button>Halal or Nah</Button>
+      </section>
     </div>
   )
 }
--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -1,18 +1,31 @@
 'use client'
 
-// TradingViewWidget.jsx
-import React, { useEffect, useRef, memo } from 'react';
+import React, { useRef, memo } from 'react';
+import { cn } from '@/lib/utils';
+import useTradingViewWidget from '@/hooks/useTradingViewWidget';
 
 // Source-- https://www.tradingview.com/widget-docs/widgets/charts/advanced-chart/
 
 
-const TradingViewWidget = () => {
-  const container = useRef(null);
+interface TradingViewWidgetProps{
+  title?: string;
+  scriptUrl: string;
+  config: Record<string, unknown>
+  height?: number;
+  className: string;
+}
+
+const TradingViewWidget = ({ title, scriptUrl, config, height=600, className}: TradingViewWidgetProps) => {
+
+  const containerRef = useTradingViewWidget(scriptUrl, config, height);
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>
+    <div className="w-full">
+      {title && <h3 className='font-semibold text-2xl text-yellow-400 mb-5'>{title}</h3>}
+      <div className={cn("tradingview-widget-container", className)} ref={containerRef}>
+        <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
+        <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>
+      </div>
     </div>
   );
 }
