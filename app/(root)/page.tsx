8450cb2ebefee4712b2fa235386e3db6bda67e29
Wilson Chan <wilsonchan54@gmail.com>
2025-10-13 23:58:15 -0400

    Added timeline and quote widgets (WIP) in root home page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,8 @@ import { MARKET_OVERVIEW_WIDGET_CONFIG_ONE, MARKET_OVERVIEW_WIDGET_CONFIG_TWO } 
 
 
 const Home = () => {
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`
+
   return (
     <div className="flex min-h-screen home_wrapper">
       <section className="grid w-full gap-8 home_section">
@@ -14,7 +16,7 @@ const Home = () => {
           {/* Widge 1 */}
           <TradingViewWidget 
             title='General Markets'
-            scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js'
+            scriptUrl={`${scriptUrl}market-overview.js`}
             config={MARKET_OVERVIEW_WIDGET_CONFIG_TWO}
             className=""
             height={600}
@@ -24,8 +26,35 @@ const Home = () => {
         {/* Widget 2 */}
         <div className="md:col-span-1 xl:col-span-2">
           <TradingViewWidget 
-            title='General Markets'
-            scriptUrl='https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js'
+            title='Stock Heatmap'
+            scriptUrl={`${scriptUrl}stock-heatmap.js`}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG_TWO}
+            className=""
+            height={600}
+          />          
+        </div>
+
+        <Button>Halal or Nah</Button>
+      </section>
+
+
+      <section className="grid w-full gap-8 home_section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          {/* Widge 3 */}
+          <TradingViewWidget 
+            title='Stories'
+            scriptUrl={`${scriptUrl}timeline.js`}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG_TWO}
+            className=""
+            height={600}
+          />
+        </div>
+
+        {/* Widget 4 */}
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <TradingViewWidget 
+            title='General Quotes'
+            scriptUrl={`${scriptUrl}market-quotes.js`}
             config={MARKET_OVERVIEW_WIDGET_CONFIG_TWO}
             className=""
             height={600}
