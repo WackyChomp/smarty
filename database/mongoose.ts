e1db834b1249b9479418cc184ee0a0edca1f4a60
Wilson Chan <wilsonchan54@gmail.com>
2025-11-03 23:24:41 -0500

    Created lib auth.ts to configure better-auth / created database mongoose.ts for db connection

---
+++ database/mongoose.ts
@@ -0,0 +1,36 @@
+import mongoose, { mongo } from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  }
+}
+
+let cached = global.mongooseCache;
+
+if(!cached){
+  cached = global.mongooseCache = { conn: null, promise: null };      // reuse old connection to avoid restarting connection
+}
+
+export const connectToDatabase = async() => {
+  if(!MONGODB_URI) throw new Error('MONGODB_URI must be set within .env!!!!');
+
+  if(cached.conn) return cached.conn;
+
+  if(!cached.promise){
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false })
+  }
+
+  try{
+    cached.conn = await cached.promise;
+  }catch(err){
+    cached.promise = null;
+    throw err;
+  }
+
+  console.log(`Connecte to database ${process.env.NODE_ENV} - ${MONGODB_URI}`)
+}
+
