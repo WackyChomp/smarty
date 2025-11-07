604bec644a366711d9c1b7c26e6a6197b8cb4596
Wilson Chan <wilsonchan54@gmail.com>
2025-11-06 22:21:37 -0500

    [WIP] created inngest api route.ts / created lib inngest client.ts and functions.ts

---
+++ lib/inngest/functions.ts
@@ -0,0 +1,14 @@
+import { inngest } from "@/lib/inngest/client"
+
+export const sendSignUpEmail = inngest.createFunction(
+  { id: 'sign-up-email'},
+  { event: 'app/user.created'},
+  async ({ event, step }) => {
+    const userProfile = `
+      -Country: ${event.data.country}
+      -Investment goals: ${event.data.investmentGoals}
+      -Risk tolerance: ${event.data.riskTolerance}
+      -Preferred industry: ${event.data.preferredIndustry}
+    `
+  }
+)
