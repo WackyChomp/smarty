a2573aa8a672f07c28b106e95d94857a629e611f
Wilson Chan <wilsonchan54@gmail.com>
2025-11-11 23:59:28 -0500

    Added sendWelcomeEmail function in nodemailer index.ts

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import { WELCOME_EMAIL_TEMPLATE } from './templates';
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -7,3 +8,19 @@ export const transporter = nodemailer.createTransport({
     pass: process.env.NODEMAILER_PW!,
   }
 })
+
+export const sendWelcomeEmail = async ({ email, name, intro } : WelcomeEmailData) =>{
+  const htmlTemplate = WELCOME_EMAIL_TEMPLATE
+    .replace(`{{name}}`, name)
+    .replace(`{{intro}}`, intro);
+
+  const mailOptions = {
+    from `"Smarty <smartykart@gimpmail.com`,
+    to: email,
+    subject: `Welcome to smarty - your personalized toolkit is ready`,
+    text: `Thanks for joining Smarty`,
+    html: htmlTemplate,
+  }
+
+  await transporter.sendMail(mailOptions);
+}
