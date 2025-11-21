642456dac112ac0e74e071e394a47707d32979d8
Wilson Chan <wilsonchan54@gmail.com>
2025-11-20 23:14:26 -0500

    Added redirect in auth layout.tsx / created lib actions user.actions.ts to be called by inngest functions.ts

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,6 +1,7 @@
 import { inngest } from "@/lib/inngest/client"
 import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts"
 import { sendWelcomeEmail } from "../nodemailer"
+import { getAllUsersForNewsEmail } from "../actions/user.actions"
 
 export const sendSignUpEmail = inngest.createFunction(
   { id: 'sign-up-email'},
@@ -40,5 +41,13 @@ export const sendSignUpEmail = inngest.createFunction(
       success: true,
       message: 'Welcome email sent successfully'
     }
+  }
+)
+
+export const sendDailyNewsSummary = inngest.createFunction(
+  { id: 'daily-news-summary' },
+  [{ event: 'app/send.daily.news' }, { cron: '0 12 * * *'}],
+  async ({ step }) => {
+    const users = await step.run('get-all-users', getAllUsersForNewsEmail)    // step 1
   }
 )
--- middelware/index.ts
+++ middelware/index.ts
@@ -15,5 +15,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/dashboard"], // Specify the routes the middleware applies to
+	// Specify the routes the middleware applies to
+	matcher:[
+		'/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up|assets).*)'
+	]
 };
